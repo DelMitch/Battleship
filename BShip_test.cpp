5a65fcedf78eb1b42a6ce29be711c17953931769
Addeline Mitchell <amitchell@alaska.edu>
2016-02-23 13:55:56 -0900

    Added two tests and intro prototype.

--- BShip_test.cpp
+++ BShip_test.cpp
@@ -11,13 +11,21 @@ TEST_CASE("Board Initialization", "[board]")
 	for (int i = 0; i < 10; ++i)
 	{
 		for (int ii = 0; ii < 10; ++ii)
-		{ REQUIRE(b.waves[i][ii] == " ~"); }
+		{
+			REQUIRE(b.waves[i][ii] == " ~");
+			REQUIRE(b.myWaters[i][ii] == " ~");
+		}
 	}
+
+	REQUIRE(b.isShip(0, 0) == false);
+	
+	b.myWaters[3][2] = " +";
+	REQUIRE(b.isShip(3, 2) == true);
 
 	// to test the print function, I put it here
 	// because I didn't know how to use REQUIRE on it
-	b.waves[4][7] = " #"; // suggested symbol for 'hit'
-	b.waves[2][0] = " @"; // suggested symbol for 'miss' (it looks like a ripple)
+	b.waves[4][7] = " X"; // suggested symbol for 'hit'
+	b.waves[2][0] = " O"; // suggested symbol for 'miss' (it looks like a ripple)
 
 	cout << "\n       0 1 2 3 4 5 6 7 8 9" << endl;
 	cout << "      _____________________" << endl;
@@ -29,4 +37,26 @@ TEST_CASE("Board Initialization", "[board]")
 		cout << " |" << endl;
 	}
 	cout << "      ---------------------" << endl;
+
+
+	cout << "                                      |__\n"
+		 << "                                      |\\/\n"
+		 << "                                      ---\n"
+		 << "                                      / | [\n"
+		 << "                              !       | |||\n"
+		 << "                            _/|     _ /|-++'\n"
+		 << "                        +  +--|    |--|--|_ |-\n"
+		 << "                     { /|__|  |/\\_ _|  |--- |||__/'\n"
+		 << "                    +---------------___[}-_===_.'____                  /\\\n"
+		 << "                ____`-' ||___-{]_| _[}-  |     |_[___\==--              \\/   _\n"
+		 << " __..._____--==/___]_|__|_____________________________[___\ == --____,------' .7\n"
+		 << "|                                                                    BB - 61 /'\n"
+		 << " \\__________________________________________________________________________|'" << endl;
+
+	cout << "__________    ____________________________.____     ___________ _________ ___ ___ ._____________ \n"
+		 << "\\______   \\  /  _  \\__    ___ /\\__    ___/|    |    \\_   _____//   _____//   |   \\|   \\______   \\ \n"
+		 << " |    |  _/ /  /_\\  \\|    |      |    |   |    |     |    __)_ \\_____  \\/    ~    \\   ||     ___/\n"
+		 << " |    |   \\/    |    \\    |      |    |   |    |___  |        \\/        \\    Y    /   ||    |\n"
+		 << " |______  /\\____|__  /____|      |____|   |_______ \\/_______  /_______  /\\___|_  /|___||____|\n"
+		 << "        \\/          \\/                            \\/        \\/        \\/       \\/" << endl;
 }
