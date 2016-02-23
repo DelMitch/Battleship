b2cd774fdb25e2e68a4ffa824c81aedb6f72738f
Addeline Mitchell <amitchell@alaska.edu>
2016-02-23 13:55:24 -0900

    Updated lists of functions and header files.

--- Board.h
+++ Board.h
@@ -5,22 +5,33 @@
 #include <cstdlib>
 #include <iostream>
 using std::cout;
+using std::cin;
 using std::endl;
 #include <vector>
 using std::vector;
 #include <string>
 using std::string;
+using std::getline;
 
 class board
 {
 	public:
 		vector<vector<string>> waves; // string for visual formatting of board
+		vector<vector<string>> myWaters;
+		//int xCoord, yCoord;
 
 	public:
 		board();
 		~board();
 
-		void printBoard(board ocean);
+		void start();
+		void setBoard(int);
+		bool isShip(int, int);
+		void printBoard();
+
+	private:
+		bool hit;
+		int x, y;
 };
 
 #endif
