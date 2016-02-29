94dfe3a66dc82be7c59c1129467ded04e723e75c
Addeline Mitchell <amitchell@alaska.edu>
2016-02-29 01:47:37 -0900

    Updated function list.

--- Board.h
+++ Board.h
@@ -31,15 +31,9 @@ using std::getline;
 class board
 {
 	public:
-<<<<<<< HEAD
 		// string for visual formatting of board
 		vector<vector<string>> waves; // hides ships
 		vector<vector<string>> myWaters; // with ships
-=======
-		vector<vector<string>> waves; // string for visual formatting of board
-		vector<vector<string>> myWaters;
-		//int xCoord, yCoord;
->>>>>>> f9faa63662b0c86a11129e940bdc2f4b1f31bd8a
 
 	public:
 		// ctor
@@ -50,7 +44,6 @@ class board
 		// documentation with implementation
 		~board();
 
-<<<<<<< HEAD
 		// intro
 		// documentation with implementation
 		const void intro();
@@ -80,17 +73,6 @@ class board
 
 	private:
 		bool secondC = false; // secondC to keep track of 2nd cruiser in setup
-=======
-		void intro();
-		void start();
-		void setBoard(int);
-		bool isShip(int, int);
-		void printBoard();
-
-	private:
-		bool hit;
-		int x, y;
->>>>>>> f9faa63662b0c86a11129e940bdc2f4b1f31bd8a
 };
 
 #endif
