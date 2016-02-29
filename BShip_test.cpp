82b9cb47ec62ceef8632c00f5e86b578ed32d78e
Addeline Mitchell <amitchell@alaska.edu>
2016-02-29 01:12:31 -0900

    Input bugs fixed.

--- BShip_test.cpp
+++ BShip_test.cpp
@@ -1,3 +1,13 @@
+// BShip_test.cpp
+// Dain Harmon
+// Addeline Mitchell
+// Mar 1, 2016
+//
+// Battleship Project
+// Group 7
+//
+// Test file for the 'board' class.
+
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
@@ -5,28 +15,104 @@
 #include <cstdio>
 #include <cstdlib>
 
-TEST_CASE("Board Initialization", "[board]")
+board b;
+
+void printB(board); // test print function prototype
+
+// printB
+// prints the board
+void printB(board b)
 {
-	board b;
-	for (int i = 0; i < 10; ++i)
-	{
-		for (int ii = 0; ii < 10; ++ii)
-		{ REQUIRE(b.waves[i][ii] == " ~"); }
-	}
-
-	// to test the print function, I put it here
-	// because I didn't know how to use REQUIRE on it
-	b.waves[4][7] = " #"; // suggested symbol for 'hit'
-	b.waves[2][0] = " @"; // suggested symbol for 'miss' (it looks like a ripple)
-
 	cout << "\n       0 1 2 3 4 5 6 7 8 9" << endl;
 	cout << "      _____________________" << endl;
 	for (int i = 0; i < 10; ++i)
 	{
-		cout << "  " << i << "  |"; 
+		cout << "  " << i << "  |";
 		for (int ii = 0; ii < 10; ++ii)
-		{ cout << b.waves[i][ii]; }
+		{ cout << b.myWaters[i][ii]; }
 		cout << " |" << endl;
 	}
 	cout << "      ---------------------" << endl;
+}
+
+
+// creation test //
+TEST_CASE("Board Initialization", "[board]")
+{
+	//b.start();
+
+	// board initialization tests //
+
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int ii = 0; ii < 10; ++ii)
+		{
+			REQUIRE(b.waves[i][ii] == " ~");
+			REQUIRE(b.myWaters[i][ii] == " ~");
+		}
+	}
+}
+
+
+// overlap tests //
+TEST_CASE("Ships Overlapping", "[board]")
+{
+	b.myWaters[4][7] = " X"; // suggested symbol for 'hit'
+	b.myWaters[2][0] = " O"; // suggested symbol for 'miss'
+	b.myWaters[3][2] = " #"; // boat?
+	
+	REQUIRE(b.isShip(3, 2) == true);
+}
+
+TEST_CASE("Ships Not Overlapping", "[board]")
+{
+	REQUIRE(b.isShip(0, 0) == false);
+}
+
+
+// test for removal of all ships //
+TEST_CASE("Reset Board", "[board]")
+{
+	b.resetWaters();
+
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int ii = 0; ii < 10; ++ii)
+		{
+			REQUIRE(b.myWaters[i][ii] == " ~");
+		}
+	}
+}
+
+
+// ship placement test //
+TEST_CASE("Interactive Setup", "[board]")
+{
+	printB(b);
+	b.setBoard(5);
+	printB(b);
+
+	b.setBoard(4);
+	printB(b);
+
+	b.setBoard(3);
+	printB(b);
+
+	b.setBoard(3);
+	printB(b);
+
+	b.setBoard(2);
+	printB(b);
+
+	int shipCount = 0;
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int ii = 0; ii < 10; ++ii)
+		{
+			if (b.myWaters[i][ii] != " ~")
+			{ ++shipCount; }
+		}
+	}
+
+	REQUIRE(shipCount == 17);
 }
